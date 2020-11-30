4f12bd18e3d740317b949826a6615d92c4898161
Jiankun YANG <JYang53@slb.com>
2020-11-30 13:37:43 +0800

    feat: should decrease counter by 1 when call decrease

--- src/app/models/counter.spec.ts
+++ src/app/models/counter.spec.ts
@@ -4,4 +4,43 @@ describe('Counter', () => {
   it('should create an instance', () => {
     expect(new Counter()).toBeTruthy();
   });
+
+  it('should set account to 10 when create an instance', () => {
+    // given
+    const counter = 10;
+
+    // when
+    const test = new Counter(counter);
+
+    // then
+    expect(test.counter).toBe(10);
+  });
+
+  it('should set account when create an instance', () => {
+    expect(new Counter().counter).toBe(0);
+  });
+
+  it('should increase counter by 1 when call increase', () => {
+    // given
+    const counter = 0;
+    const test = new Counter(counter);
+
+    // when
+    test.increase();
+
+    // then
+    expect(test.counter).toBe(counter + 1);
+  });
+
+  it('should decrease counter by 1 when call decrease', () => {
+    // given
+    const counter = 0;
+    const test = new Counter(counter);
+
+    // when
+    test.decrease();
+
+    // then
+    expect(test.counter).toBe(counter - 1);
+  });
 });
--- src/app/models/counter.ts
+++ src/app/models/counter.ts
@@ -1,2 +1,15 @@
 export class Counter {
+  public counter: number;
+
+  constructor(counter: number = 0) {
+    this.counter = counter;
+  }
+
+  public increase(): void {
+    this.counter ++;
+  }
+
+  public decrease(): void {
+    this.counter --;
+  }
 }
